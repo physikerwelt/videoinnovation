7800ab9451f2a93b593b5b293f2591798ac65dcf
swtu <sebastian.wiese@campus.tu-berlin.de>
2013-07-21 13:58:01 +0200

    upload script fix

--- www/upload_file.php
+++ www/upload_file.php
@@ -18,17 +18,19 @@ $height = $_POST["height"];
 
 
 if ($video["error"] > 0 && $pdf["error"] > 0 && $transcription["error"] > 0) {
+	echo 'error codes: video:'.$video["error"]."- pdf: ".$pdf["error"]."- transcription: ".$transcription["error"];
+} else {
 	$folderName = basename($video["name"],".mov");
 	mkdir($_SERVER['DOCUMENT_ROOT']."/working/".$folderName);
 	$path = $_SERVER['DOCUMENT_ROOT']."/working/".basename($video["name"],".mov");
 	
-	move_uploaded_file($video["temp_name"], $path."VL01.mov");
-	move_uploaded_file($pdf["temp_name"], $path."pdf.pdf");
-	move_uploaded_file($transcription["temp_name"], $path."transcription.json");
+	move_uploaded_file($video["tmp_name"], $path."/VL01.mov");
+	move_uploaded_file($pdf["tmp_name"], $path."/pdf.pdf");
+	move_uploaded_file($transcription["tmp_name"], $path."/transcription.json");
 	$config = '{"path":'.$path.', "x":'.$x.',"y":'.$y.',"width":'.$width.',"height":'.$height.',"agenda":'.$agenda.',"pdfLink":'.$pdfLink.'}';
-	file_put_contents($path."configuration.json",$config);
+	file_put_contents($path."/configuration.json",$config);
 	
-	exec("VideoSkript.bat ".$folderName);
+	exec("VideoSkript.bat ".$folderName);	
 }
 
 ?> 
